01649281921d76aba2dab2faf15727f974007c14
LifelessFPS <lifelessfps2549@gmail.com>
2025-01-13 14:30:37 +0700

    LabSheet6Practice

---
+++ LabSheet6_ClassAndObject/src/TestLoancalculator.java
@@ -0,0 +1,15 @@
+
+public class TestLoancalculator {
+
+	public static void main(String[] args) {
+		LoanCalculator loan = new LoanCalculator();
+		
+		loan.setLoanDetails("Laptop",50000, 5, 2);
+		
+		loan.displayLoanDetails();
+		
+		
+
+	}
+
+}
