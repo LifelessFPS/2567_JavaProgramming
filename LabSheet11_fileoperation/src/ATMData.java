db709df2e1bbab162740f19452657ca59cc2fb0b
LifelessFPS <lifelessfps2549@gmail.com>
2025-02-24 14:23:17 +0700

    LabSheet11Practise

--- LabSheet11_fileoperation/src/ATMData.java
+++ LabSheet11_fileoperation/src/ATMData.java
@@ -1,4 +1,20 @@
 
 public class ATMData {
+	private String accountNumber;
+	private String password;
+	
+	ATMData(String accountNumber,String password) {
+		this.accountNumber = accountNumber;
+		this.password = password;
+	}
+	
+	String getId() {
+		return this.accountNumber;
+	}
+	
+	String getPass() {
+		return this.password;
+	}
 
+	
 }
